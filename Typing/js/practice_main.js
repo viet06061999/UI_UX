4fd91fee29e068aacebf48ac864c5b4ec4491f42
Ba Son PHAM <bspham@linagora.com>
2020-12-10 16:54:29 +0700

    abc

---
+++ Typing/js/practice_main.js
@@ -0,0 +1,52 @@
+
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+var handler = function (e) { 
+    handler.data.push(e);
+    console.log(handler.data);
+}
+handler.data = [];
+
+function recordClickKeyboard() {
+    console.log(window.addEventListener("keydown",  handler));
+}
+
+recordClickKeyboard();
+
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
+
+//hide keyboard
+function hide() {
+    var keyboard = document.getElementById("keyboardWrapper");
+    if (keyboard.style.display === "none") {
+        keyboard.style.display = "flex";
+    } else {
+        keyboard.style.display = "none";
+    }
+}
+
+function nextWord() {
+    var word = document.getElementById("practiceWord");
+    if ( word.innerHTML == "A"){
+        word.innerHTML = "B"
+    }else{
+        word.innerHTML = "A"
+    }
+}
+
